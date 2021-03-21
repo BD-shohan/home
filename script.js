e51578298031bc1b6c1b246b57c7bf4c397231cd
Shohanur Rahman <80466653+BD-shohan@users.noreply.github.com>
2021-03-22 00:07:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var wij = document.getElementById("b-1")
+wij.addEventListener("click", function b1(){
+ document.getElementById("head").innerHTML= "What is java-Script ?"
+ document.getElementById("main-img").src = "coding.jpeg";
+  document.getElementById("none").innerHTML= "JavaScript is a text~based programming language used both on the client-side and server-side that allows you to make web pages interactive. Where HTML and CSS are languages that give structure and style to web pages, JavaScript gives web pages interactive elements that engage a usear Fact ~ This website also made in JavaScript"
+  })
+ 
+  document.getElementById("b-2").addEventListener("click", function b2(){
+ document.getElementById("head").innerHTML= " How i start learning Web development"
+ document.getElementById("main-img").src = "coding.jpeg";
+  document.getElementById("none").innerHTML= "I was thinking how to learn programming language and suddenly i found a video of jankar mahbub . In this video i learned the basic of webdevelopment and he suggested w3school to learn other staff and i learn HTML from there and i Feel damn its so interesting 💀 and still now i am learning 🐼 "
+  })
+  
+  
+  document.getElementById("b-3").addEventListener("click", function t1(){
+ document.getElementById("head").innerHTML= " How long it will take to learn HTML "
+ document.getElementById("main-img").src = "coding.jpeg";
+  document.getElementById("none").innerHTML= "There’s never been a better time to start learning HTML. If you’re interested in a career in web design or web development—or if you just want to know how to make great web pages—a basic knowledge of HTML is a must. HTML really has no competition as a coding language and is accepted as the industry standard.Fortunately, the basics of HTML are actually pretty easy for the average learner to grasp. You can start picking up HTML in a matter of hours. It should take you one to two weeks to get the full gist of HTML, and about a month of practice to get comfortable with the language. The key is to apply your learning by working on projects.This article takes a brief look at the history of HTML, why it remains so relevant today, and how you can build up a workable knowledge of HTML quickly and in a manner that suits your lifestyle. So let’s dive in."
+  })
+  
+  document.getElementById("b-4").addEventListener("click", function b4(){
+ document.getElementById("head").innerHTML= " How long it will take to learn JavaScript "
+ document.getElementById("main-img").src = "coding.jpeg";
+  document.getElementById("none").innerHTML= " If you are new to coding, you may wonder if you can get a grasp of a programming language within weeks. The answer is simply: no. It’s next to impossible to gain expert knowledge of all the programming languages in demand and obtain a junior developer job in such a short amount of time.Luckily, many companies will hire junior developers who are still gaining proficiency in certain in-demand languages. One of those languages is JavaScript.JavaScript is the most common language for making webpages interactive. To really grab the attention of visitors to your website, you will want it to have interactive features.But how long will it really take to learn the basics of JavaScript? Let’s dive in and see."
+  })
+  document.getElementById("t-1").addEventListener("click", function t1(){
+ document.getElementById("head").innerHTML= " 24 mar Sunday - 2021 "
+ document.getElementById("main-img").src = "t1.jpeg";
+  document.getElementById("none").innerHTML= "I spend hole day building this website ! NOTHING SPECIAL HAPPENED 😭 Yeh this website is special I know 🤣 my target was building something else ! But i ended up here 🤣🤣 BTW its so hard to code in a phone for screen size . And also i can't use keyboard ... So ot was a result of hard-coding 😅 . BTW if you want Source code you can contact me at shohantj@gmail.com"
+  })
+  document.getElementById("t-2").addEventListener("click", function t1(){
+ document.getElementById("head").innerHTML= " Story of my First Relationship ?"
+ document.getElementById("main-img").src = "t2.jpeg";
+  document.getElementById("none").innerHTML= "I Respect your interest ! But Still its under development ! Please wait some days ... I will try my level best to upload this contact as soon as i can and again thanks for your interest 😊"
+  })
+  
